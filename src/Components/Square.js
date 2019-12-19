580c55e7f917ce939253e55bede469afefb581be
snyarde <57542514+snyarde@users.noreply.github.com>
2019-12-19 15:09:29 -0500

    Convert to arrow function
    Convert to arrow function syntax

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -3,12 +3,7 @@ import React, { Component } from "react";
 class Square extends Component {
   render() {
     return (
-      <button
-        className="square"
-        onClick={function() {
-          alert("click");
-        }}
-      >
+      <button className="square" onClick={() => alert("click")}>
         {this.props.value}
       </button>
     );
