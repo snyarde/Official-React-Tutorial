0e5b1b78628b70e85dd28abb776a03fac697a25c
snyarde <57542514+snyarde@users.noreply.github.com>
2019-12-19 15:04:14 -0500

    Passing Data Through Props
    [Passing Data Through Props](https://reactjs.org/tutorial/tutorial.html#passing-data-through-props)

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Square extends Component {
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return <button className="square">{this.props.value}</button>;
   }
 }
 
