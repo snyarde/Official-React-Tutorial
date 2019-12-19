f47360cddba44b3d861cf11fe9015792cdc8ed76
snyarde <57542514+snyarde@users.noreply.github.com>
2019-12-19 14:54:08 -0500

    Refactors single source
    Refactors single source file from example into a single file per component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import Game from "./Components/Game";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Yo</h1>
+        <Game />
       </div>
     );
   }
