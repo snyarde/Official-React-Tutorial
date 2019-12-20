f700e0dae2f601ce238fe5774609e6f1bbea6e0e
snyarde <57542514+snyarde@users.noreply.github.com>
2019-12-20 11:16:39 -0500

    Adds logic to take turns
    Adds logic to take turns [Taking Turns](https://reactjs.org/tutorial/tutorial.html#taking-turns)

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -6,14 +6,18 @@ class Board extends Component {
     super(props);
     this.state = {
       // Set boards initial state to an array of nulls corresponding to each square
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = "X";
-    this.setState({ squares: squares });
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
   }
 
   renderSquare(i) {
