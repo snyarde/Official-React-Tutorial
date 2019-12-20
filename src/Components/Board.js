2f390e5955d5dff4869d2018ce8a5c65562ae77d
snyarde <57542514+snyarde@users.noreply.github.com>
2019-12-20 09:44:02 -0500

    Adds Square state in Board component
    Adds Square state in Board component and passes into square on creation

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -2,8 +2,16 @@ import React, { Component } from "react";
 import Square from "./Square";
 
 class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // Set boards initial state to an array of nulls corresponding to each square
+      squares: Array(9).fill(null)
+    };
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value={this.state.squares[i]} />;
   }
 
   render() {
