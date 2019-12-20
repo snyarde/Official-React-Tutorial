99e93181c0b9aae853e85813f54fb579057635ac
snyarde <57542514+snyarde@users.noreply.github.com>
2019-12-20 09:52:09 -0500

    Updates Square component to lift state up to Board
    Updates Square component to lift state up to Board by removing local state and calling function passed in as props on click

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -1,17 +1,10 @@
 import React, { Component } from "react";
 
 class Square extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null
-    };
-  }
-
   render() {
     return (
-      <button className="square" onClick={() => this.setState({ value: "X" })}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
