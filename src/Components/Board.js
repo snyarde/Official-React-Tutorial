30eef012813efa034d649505c1c2d4e7ce1ed8a8
snyarde <57542514+snyarde@users.noreply.github.com>
2019-12-20 11:19:40 -0500

    Displays next player's turn indication
    Displays next player's turn indication to the UI

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -30,7 +30,7 @@ class Board extends Component {
   }
 
   render() {
-    const status = "Next player: X";
+    const status = "Next player: " + (this.state.xIsNext ? "X" : "O");
 
     return (
       <div>
